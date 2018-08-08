56f87f32219d48a26f202ac6d320428c7b2030fe
doteve43 <doteve43@gmail.com>
2018-08-08 23:00:45 +0800

    add viewcontroller

---
+++ Quiz/Quiz/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  Quiz
+//
+//  Created by 阎斌 on 2018/8/8.
+//  Copyright © 2018年 yb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
